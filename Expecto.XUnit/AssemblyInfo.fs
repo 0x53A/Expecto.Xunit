6edae000b17eced3e00b5207aa2ade602028af37
Lukas Rieger <lr@live.de>
2017-03-02 01:29:17 +0100

    yay

--- Expecto.XUnit/AssemblyInfo.fs
+++ Expecto.XUnit/AssemblyInfo.fs
@@ -8,11 +8,11 @@ open System.Runtime.InteropServices
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("Expecto.XUnit")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("Run Expecto tests with the xunit runner")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Expecto.XUnit")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyCopyright("Copyright © Lukas Rieger 2017")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.1.1.0")>]
+[<assembly: AssemblyFileVersion("0.1.1.0")>]
 
 do
     ()
